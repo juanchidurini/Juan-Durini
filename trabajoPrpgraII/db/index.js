37c786e91e10bacab0d5f23f6dc95dbed85b9a22
juanchidurini <jdurini@udesa.edu.ar>
2025-04-14 21:37:16 -0300

    cambie los productos y los comentarios

--- trabajoPrpgraII/db/index.js
+++ trabajoPrpgraII/db/index.js
@@ -17,19 +17,19 @@ let data = {
             descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
             comentario: [
                 {
-                    nombre: "ninja master",
-                    comentario: "muy buena cafetera",
-                    foto: "/images/users/default-image.png"
+                    nombre: "juyanchi600",
+                    comentario: "me gusta el cafe que hace",
+                    foto: "/images/users/img-dino.png"
                 },
                 {
-                    nombre: "ninja master",
-                    comentario: "muy buena cafetera",
-                    foto: "/images/users/default-image.png"
+                    nombre: "cafecito1234",
+                    comentario: "muy futuristica",
+                    foto: "/images/users/img-mario.png"
                 },
                 {
-                    nombre: "ninja master",
-                    comentario: "muy buena cafetera",
-                    foto: "/images/users/default-image.png"
+                    nombre: "cebollin",
+                    comentario: "los lattes medio flojos",
+                    foto: "/images/users/img-cebolla.png"
                 }
             ]
         },
@@ -41,17 +41,17 @@ let data = {
             comentario: [{
                 nombre: "ninja master",
                 comentario: "alta compu",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-ninja.png"
             },
             {
                 nombre: "dino",
                 comentario: "no me llego nunca",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-dino.png"
             },
             {
                 nombre: "Graciela",
                 comentario: "a mi nieto le gusta mucho",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-abuela.png"
             }
             ]
         },
@@ -61,19 +61,19 @@ let data = {
             nombre: "Galaxy s10",
             descripcion: "Samsung Galaxy s10",
             comentario: [{
-                nombre: "ninja master",
+                nombre: "terminator10",
                 comentario: "me encata este celu",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-dino.png"
             },
             {
                 nombre: "mauri",
                 comentario: "prefiero apple",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-mario.png"
             },
             {
                 nombre: "manzana 123",
                 comentario: "Cuanto dura la bateria?",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-cebolla.png"
             }
             ]
         },
@@ -85,7 +85,7 @@ let data = {
             comentario: [{
                 nombre: "siri",
                 comentario: "me encatnta ver la tele",
-                foto: "/images/users/default-image.png"
+                foto: "/images/users/img-ninja.png"
             },
             {
                 nombre: "user6325",
@@ -94,140 +94,140 @@ let data = {
             },
             {
                 nombre: "German",
-                comentario: "no se que comentar",
-                foto: "/images/users/default-image.png"
+                comentario: "las pelis se ven increibles",
+                foto: "/images/users/img-dino.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-casio.png",
             id: "5",
             nombre: "Reloj Casio",
             descripcion: "Reloj pulsera Casio",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
+                nombre: "mariana",
+                comentario: "Es sumergible?",
                 foto: "/images/users/default-image.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "pablo",
+                comentario: "que buen reloj!",
+                foto: "/images/users/img-abuela.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "tati",
+                comentario: "pense que era mas grande",
+                foto: "/images/users/img-mario.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-alexa.png",
             id: "6",
-            nombre: "Cafetera Moulinex",
-            descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
+            nombre: "Alexa",
+            descripcion: "Asistente inteligenta Alexa",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "chiki",
+                comentario: "es justo lo que necesitaba",
+                foto: "/images/users/img-cebolla.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "pipe",
+                comentario: "me gusta charlar con ella",
+                foto: "/images/users/img-dino.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "tomi",
+                comentario: "me sirve para escuhcar musica",
+                foto: "/images/users/img-ninja.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-timbre.png",
             id: "7",
-            nombre: "Cafetera Moulinex",
-            descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
+            nombre: "Timbre inalámbrico",
+            descripcion: "Timbre inalámbrico a distancia de plastico.",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "plankton",
+                comentario: "me resolvio mis problemas",
+                foto: "/images/users/img-dino.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "messi",
+                comentario: "cunato dura a bateria?",
+                foto: "/images/users/img-messi.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "pata72",
+                comentario: "viejisimo el modelo",
+                foto: "/images/users/img-cebolla.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-kitchen-aid.png",
             id: "8",
-            nombre: "Cafetera Moulinex",
-            descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
+            nombre: "Kitchen Aid",
+            descripcion: "Batidora electrica de cocina.",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "agustina",
+                comentario: "precio",
+                foto: "/images/users/img-abuela.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "alejandra",
+                comentario: "amo esta batidora!!!!!",
+                foto: "/images/users/img-cebolla.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "perseo",
+                comentario: "muy buena batidora",
+                foto: "/images/users/img-mario.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-teclado.png",
             id: "9",
-            nombre: "Cafetera Moulinex",
-            descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
+            nombre: "Teclado Yamaha",
+            descripcion: "Teclado Yamaha de 7 octavas",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "astronauta",
+                comentario: "se lo compre a mi hijo y le encanto",
+                foto: "/images/users/img-dino.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "cazador mario",
+                comentario: "me encanta tocar el piano",
+                foto: "/images/users/img-mario.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "lopez16",
+                comentario: "lo probe en vivo y me gusta como se mimetiza con los otros instrumentos",
+                foto: "/images/users/img-ninja.png"
             }
             ]
         },
         {
-            foto: "img-cafetera-moulinex.jpg",
+            foto: "img-sonos.png",
             id: "10",
-            nombre: "Cafetera Moulinex",
-            descripcion: "Cafetera para hacer cafe muy rico de excelente calidad.",
+            nombre: "Sonos 300",
+            descripcion: "Parlante Sonos 300",
             comentario: [{
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "gonzalo barrios",
+                comentario: "muy buena parlante, que potenica",
+                foto: "/images/users/img-abuela.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "margaritaperez_",
+                comentario: "muy buenos graves",
+                foto: "/images/users/img-cebolla.png"
             },
             {
-                nombre: "ninja master",
-                comentario: "muy buena cafetera",
-                foto: "/images/users/default-image.png"
+                nombre: "Edgar.t",
+                comentario: "mejor parlante precio-calidad",
+                foto: "/images/users/img-mario.png"
             }
             ]
         },
